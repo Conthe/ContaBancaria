f5a765fac49a69e0328b45be0fcfec7be07d8605
Rafael Silva Aleixo <rafael.aleixo@vericode.com.br>
2021-12-28 00:25:17 -0300

    Resolucao de exercicio sobre conta bancaria e suas acoes

---
+++ CriacaoContaBancaria/ContaBancaria.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriacaoContaBancaria
+{
+    public class ContaBancaria
+    {
+        public string NumeroConta { get; set; }
+        public string TitularConta { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria(string numeroConta, string titularConta)
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+        }
+
+        public ContaBancaria(string numeroConta, string titularConta, double saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+            Saldo = saldoInicial;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + NumeroConta + ", Titular: " + TitularConta + ", Saldo: $" + Saldo.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        internal void Depositar()
+        {
+            Console.Write("Entre um valor para depósito: ");
+            double valorParaDeposito = double.Parse(Console.ReadLine());
+            Saldo += valorParaDeposito;
+        }
+
+        internal void Sacar()
+        {
+            Console.Write("Entre um valor para saque: ");
+            double valorParaSaque = double.Parse(Console.ReadLine());
+            valorParaSaque += 5;
+            Saldo -= valorParaSaque;
+        }
+
+        internal void MostrarDadosConta()
+        {
+            Console.WriteLine("=======================================");
+
+            Console.WriteLine("Dados da conta");
+
+            Console.WriteLine(this.ToString());
+
+            Console.WriteLine("=======================================");
+        }
+    }
+}
---
+++ CriacaoContaBancaria/CriacaoConta.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CriacaoContaBancaria
+{
+    class CriacaoConta
+    {
+        static void Main(string[] args)
+        {
+            string numeroConta, titular, depositoInicial;
+            ContaBancaria NovaConta;
+
+            InfoConta(out numeroConta, out titular, out depositoInicial);
+            CriarConta(numeroConta, titular, out depositoInicial, out NovaConta);
+
+            NovaConta.MostrarDadosConta();
+
+            NovaConta.Depositar();
+
+            NovaConta.MostrarDadosConta();
+
+            NovaConta.Sacar();
+
+            NovaConta.MostrarDadosConta();
+        }
+
+        private static void CriarConta(string numeroConta, string titular, out string depositoInicial, out ContaBancaria NovaConta)
+        {
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            depositoInicial = Console.ReadLine();
+            if (depositoInicial == "s")
+            {
+                Console.Write("Entre o valor  de depósito inicial: ");
+                double saldoInicial = double.Parse(Console.ReadLine());
+                NovaConta = new ContaBancaria(numeroConta, titular, saldoInicial);
+            }
+            else
+            {
+                NovaConta = new ContaBancaria(numeroConta, titular);
+            }
+        }
+
+        private static void InfoConta(out string numeroConta, out string titular, out string depositoInicial)
+        {
+            numeroConta = string.Empty;
+            titular = string.Empty;
+            depositoInicial = string.Empty;
+            Console.Write("Entre o número da conta: ");
+            numeroConta = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            titular = Console.ReadLine();
+        }
+    }
+}
